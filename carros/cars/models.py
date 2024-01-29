1b13bec4991558744c520593a9f6edba371609d9
Erick Bryan Cubas <thecubas@outlook.com>
2024-01-28 22:09:38 -0300

    commit -m "modifications in settings and models"
    - car object(2)
    - translate pt-br and America/Sao_Paulo

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -24,3 +24,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/',blank=True, null=True)
+    
+    def __str__(self):
+        return self.model
+    
