9e25cb2a77aa14c19355adf4c4f3b4b6eef74070
Erick Bryan Cubas <thecubas@outlook.com>
2024-01-29 23:00:07 -0300

    commit -m "Adicionando uma view Car"

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def cars_view(request):
+    html = '''
+        <html>
+            <head>
+                <title>Meus Carros</title>
+            </head>
+            <body>
+                <h1>Carros da PyCodeBR</h1>
+            </body>
+        </html>
+    
+    '''
+    return HttpResponse(html)
