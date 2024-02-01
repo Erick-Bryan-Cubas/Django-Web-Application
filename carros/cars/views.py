ca742ab085dd16eb58ce03fe493ecc0079c375a2
Erick Bryan Cubas <thecubas@outlook.com>
2024-01-31 23:32:37 -0300

    commit -m "Fazendo consultas com Filter()"

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = "hello"
-    #cars = Car.objects.filter(brand=0)
+    # cars = Car.objects.filter(brand=3)
+    # cars = Car.objects.filter(brand__name='Chevrolet')
+    # cars = Car.objects.filter(model='Chevette Tubarão')
+    cars = Car.objects.filter(model__contains='Chevette')
     print(cars)
     
     
