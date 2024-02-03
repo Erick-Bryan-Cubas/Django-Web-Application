300ead1ff4ab3946ddd05979b07996adc08a6121
Erick Bryan Cubas <thecubas@outlook.com>
2024-02-03 16:34:26 -0300

    commit -m "Filtrando com parâmetros do usuário"

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    print(request) # Captura os parametros de query request do usuario
-    print(request.GET.get('search')) # Captura os valores da query request do usuario
-    
     cars = Car.objects.all()
+    search  = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model__contains=search)
+    
+    # print(request) # Captura os parametros de query request do usuario
+    # print(request.GET.get('search')) # Captura os valores da query request do usuario
     # cars = Car.objects.filter(brand=3)
     # cars = Car.objects.filter(brand__name='Chevrolet')
     # cars = Car.objects.filter(model='Chevette Tubarão')
