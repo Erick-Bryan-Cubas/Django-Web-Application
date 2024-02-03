f0b5cbf5d6d8d3ec54f9ace2a643344c86da8253
Erick Bryan Cubas <thecubas@outlook.com>
2024-02-03 14:32:46 -0300

    commit -m "Parâmetros da request"

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
+    print(request) # Captura os parametros de query request do usuario
+    print(request.GET.get('search')) # Captura os valores da query request do usuario
+    
+    cars = Car.objects.all()
     # cars = Car.objects.filter(brand=3)
     # cars = Car.objects.filter(brand__name='Chevrolet')
     # cars = Car.objects.filter(model='Chevette Tubarão')
-    cars = Car.objects.filter(model__contains='Chevette')
-    print(cars)
-    
-    
-    
+    # cars = Car.objects.filter(model__contains='Chevette')
+   
     return render(
         request,
         'cars.html',
