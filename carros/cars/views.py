af072997f182097445d783238482823bc332b1ff
Erick Bryan Cubas <thecubas@outlook.com>
2024-02-04 10:32:02 -0300

    commit -m "Reescrevendo cars_view (View Base)"

--- carros/cars/views.py
+++ carros/cars/views.py
@@ -1,29 +1,34 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    # cars = Car.objects.all().order_by('-model') # Ordenação invertida
-    search  = request.GET.get('search')
-    if search:
-        # cars = Car.objects.filter(model__contains=search)
-        cars = Car.objects.filter(model__icontains=search) # ignore case
-        # cars = Car.objects.filter(model__icontains=search).order_by('model') # Ordenação dos dados
-        
-    # print(request) # Captura os parametros de query request do usuario
-    # print(request.GET.get('search')) # Captura os valores da query request do usuario
-    # cars = Car.objects.filter(brand=3)
-    # cars = Car.objects.filter(brand__name='Chevrolet')
-    # cars = Car.objects.filter(model='Chevette Tubarão')
-    # cars = Car.objects.filter(model__contains='Chevette')
-   
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars}
-        )
+class CarsView(View): # A classe CarsView herda os recursos de View
     
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        # cars = Car.objects.all().order_by('-model') # Ordenação invertida
+        search  = request.GET.get('search')
+        if search:
+            # cars = Car.objects.filter(model__contains=search)
+            cars = Car.objects.filter(model__icontains=search) # ignore case
+            # cars = Car.objects.filter(model__icontains=search).order_by('model') # Ordenação dos dados
+            
+        # print(request) # Captura os parametros de query request do usuario
+        # print(request.GET.get('search')) # Captura os valores da query request do usuario
+        # cars = Car.objects.filter(brand=3)
+        # cars = Car.objects.filter(brand__name='Chevrolet')
+        # cars = Car.objects.filter(model='Chevette Tubarão')
+        # cars = Car.objects.filter(model__contains='Chevette')
+    
+        return render(
+            request,
+            'cars.html',
+            {'cars': cars}
+            )
+
+
+   
 def new_car_view(request):
     if request.method =='POST':
         new_car_form = CarModelForm(request.POST, request.FILES)
