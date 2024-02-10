56301330778f2dbf5078e6c6bcc93a0a6ca9343d
Erick Bryan Cubas <thecubas@outlook.com>
2024-02-10 19:05:13 -0300

    :sparkles: feat: Criando inventário

--- carros/cars/models.py
+++ carros/cars/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -36,3 +35,13 @@ class Car(models.Model):
     def __str__(self):
         return self.model
     
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f'{self.cars_count} carros no valor total de R$ {self.cars_value:.2f	}'
