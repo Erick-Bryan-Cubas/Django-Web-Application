4e843be0e489113d5728aa44e4ade82b6d4907c2
Erick Bryan Cubas <thecubas@outlook.com>
2024-02-10 21:03:01 -0300

    :sparkles: feat: Integrando Django com a Api da OpenIA (ChatGPT)

---
+++ carros/openai_api/client.py
@@ -0,0 +1,21 @@
+import openai
+import configparser
+import os 
+
+userprofile = os.environ['USERPROFILE']
+credentials_path = os.path.join(userprofile, 'Área de Trabalho\\Projetos\\DjangoMaster\\')
+config = configparser.ConfigParser()
+config.read(credentials_path + 'APIDjangoMaster.ini')
+
+def get_car_ai_bio(model, brand, year):
+    prompt = f'''
+    Create a bio in portuguese for a car model {model} from {brand} of the year {year} with 250 words.
+    '''
+    openai.api_key = config['API']['secret-key']
+    
+    response = openai.Completion.create(
+        model='gpt-3.5-turbo-1106',
+        prompt=prompt,
+        max_tokens=100,
+    )
+    return response['choices'][0]['text']
